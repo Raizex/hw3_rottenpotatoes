d2a8965fc8b34a50ec510fe8c8cad1cde13493dc
Justin Huffman <jwhuffman@gmail.com>
2013-11-07 18:15:01 -0800

    Implemented tests for filter

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,8 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +12,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  regexp = /#{string1}.*#{string2}/m #  /m means match across newlines
+  page.body.should =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +24,22 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/, /).each do |rating|
+    if (uncheck) then step "I uncheck \"ratings_#{rating}\""
+    else 
+      step "I check \"ratings_#{rating}\""
+    end
+  end
+end
+
+Then /I should see movies with the following ratings: (.*)/ do |rating_list|
+  rating_list.split(/, /).each do | rating |
+    Movie.find_all_by_rating(rating).each do |movie|
+      #if (nt) 
+      #  step "I should not see \"#{movie.title}\""
+      #else
+        step "I should see \"#{movie.title}\""
+      #end
+    end
+  end
 end
